0a555e2c74bec06efce58d67a3d0b293a9872d2d
chibeze <chibeze2@hotmail.co.uk>
2023-12-05 01:30:31 +0000

    Added cross origin resouse sharing to wildcard for development

--- server/server.py
+++ server/server.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 import asyncio
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 def set_event_loop_policy():
     policy = asyncio.get_event_loop_policy()
