54110a8921675d9ce0c8accb054ec15c96adaee0
LAPTOP-58J52BBF\chibe <chibeze2@hotmail.co.uk>
2024-01-18 16:44:10 +0000

    remove redundant code from app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,14 @@
 import "./App.css";
 import ChatInterface from "./components/chat/ChatInterface";
 import Header from "./components/Header";
-// import UploadFile from "./components/UploadFile";
-import Footer from "./components/Footer";
 
 function App() {
-	return (
-		<div className="App">
-			<Header />
-			<ChatInterface className="ChatInterface" />
-		</div>
-	);
+  return (
+    <div className="App">
+      <Header />
+      <ChatInterface className="ChatInterface" />
+    </div>
+  );
 }
 
 export default App;
